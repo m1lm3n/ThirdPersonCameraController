5417e87a961a4caa2c39d2c0192e55c3115fd304
Aleksandr Vinichenko <m3lman.94@gmail.com>
2022-10-13 22:12:24 +0300

    Add Project

---
+++ Assets/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,164 @@
+using System.Drawing;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    public Transform Target;
+    public int MouseButton = 1; // right button by default
+    public float Distance = 20;
+    public float MinDistance = 1;
+    public float MaxDistance = 20;
+    public float ZoomSpeedMouse = 1;
+    public float ZoomSpeedTouch = 0.2f;
+    public float RotationSpeed = 10;
+    public float xMinAngle = -90;
+    public float xMaxAngle = 90;
+    public Vector3 Camera_offset = Vector3.zero;
+    public LayerMask viewBlockingLayers;
+
+    private UnityEngine.Color cameraRayColor = UnityEngine.Color.red;
+    private float detectionRadius = 0.5f;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 targetPos = Vector3.zero;
+    private bool rotationInitialized;
+    private bool cursorLocked = false;
+    private bool cursorPositionSaved = false;
+    private MousePosition mp;
+    [DllImport("user32.dll")]
+    public static extern bool SetCursorPos(int X, int Y);
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool GetCursorPos(out MousePosition lpMousePosition);
+    public static System.Drawing.Point Position { get; set; }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MousePosition
+    {
+        public int x;
+        public int y;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitialRotation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Target != null)
+        {
+            ApplyCameraOffset();
+
+            if (!Utilites.IsCursorOverUserInterface())
+            {
+                if (Input.mousePresent)
+                {
+                    if (Input.GetMouseButton(MouseButton))
+                    {
+                        CameraRotation();
+                        LockCursor();
+                    }
+                    else
+                    {
+                        UnlockCursor();
+                    }
+                }
+                else
+                {
+                    InitialRotation();
+                }
+
+                CameraZoom();
+            }
+
+            ApplyCameraPosition();
+            CameraCollision();
+        }
+    }
+
+    private void InitialRotation()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(45, 0, 0));
+    }
+    private void CameraZoom()
+    {
+        float speed = Input.mousePresent ? ZoomSpeedMouse : ZoomSpeedTouch;
+        float step = Utilites.GetZoomUniversal() * speed;
+        Distance = Mathf.Clamp(Distance - step, MinDistance, MaxDistance);
+    }
+    private void CameraCollision()
+    {
+        Ray ray = new Ray(targetPos, -this.transform.forward);
+        RaycastHit raycastHit;
+
+        Debug.DrawLine(targetPos, transform.position, cameraRayColor);
+
+        if (Physics.SphereCast(ray, detectionRadius, out raycastHit, Distance, viewBlockingLayers))
+        {
+            float distance = Vector3.Distance(targetPos, raycastHit.point);
+
+            transform.position = targetPos - (transform.rotation * Vector3.forward * distance);
+        }
+    }
+    private void CameraRotation()
+    {
+        if (!rotationInitialized)
+        {
+            rotation = transform.eulerAngles;
+            rotationInitialized = true;
+        }
+
+        rotation.y += Input.GetAxis("Mouse X") * RotationSpeed;
+        rotation.x -= Input.GetAxis("Mouse Y") * RotationSpeed;
+        rotation.x = Mathf.Clamp(rotation.x, xMinAngle, xMaxAngle);
+        transform.rotation = Quaternion.Euler(rotation.x, rotation.y, 0);
+    }
+    private void ApplyCameraPosition()
+    {
+        transform.position = targetPos - (transform.rotation * Vector3.forward * Distance);
+    }
+    private void ApplyCameraOffset()
+    {
+        targetPos = Target.position + Camera_offset;
+    }
+    private void LockCursor()
+    {
+        cursorLocked = true;
+        UnityEngine.Cursor.visible = false;
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+        SaveCursorPosition();
+    }
+    private void UnlockCursor()
+    {
+        if (cursorLocked)
+        {
+            UnityEngine.Cursor.lockState = CursorLockMode.Confined;
+            cursorLocked = false;
+            UnityEngine.Cursor.visible = true;
+            SetCursorPosition();
+        }
+    }
+    private void SaveCursorPosition()
+    {
+        if (!cursorPositionSaved)
+        {
+            // GetCursorPos(out mp);
+            cursorPositionSaved = true;
+        }
+    }
+    private void SetCursorPosition()
+    {
+        // SetCursorPos(mp.x, mp.y);
+        cursorPositionSaved = false;
+    }
+    // Draw a Gizmo around where the camera has been projected to.
+    void OnDrawGizmos()
+    {
+        Gizmos.color = UnityEngine.Color.blue;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+    }
+}
---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Reflection;
+
+public class Utilites : MonoBehaviour
+{
+    public static bool IsCursorOverUserInterface()
+    {
+        // IsPointerOverGameObject check for left mouse (default)
+        if (EventSystem.current.IsPointerOverGameObject())
+            return true;
+
+        // IsPointerOverGameObject check for touches
+        for (int i = 0; i < Input.touchCount; ++i)
+            if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(i).fingerId))
+                return true;
+
+        // OnGUI check
+        return GUIUtility.hotControl != 0;
+    }
+    public static float GetAxisRawScrollUniversal()
+    {
+        float scroll = Input.GetAxisRaw("Mouse ScrollWheel");
+        if (scroll < 0) return -1;
+        if (scroll > 0) return  1;
+        return 0;
+    }
+    public static float GetPinch()
+    {
+        if (Input.touchCount == 2)
+        {
+            // Store both touches.
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Find the position in the previous frame of each touch.
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            return touchDeltaMag - prevTouchDeltaMag;
+        }
+        return 0;
+    }
+    public static float GetZoomUniversal()
+    {
+        if (Input.mousePresent)
+            return GetAxisRawScrollUniversal();
+        else if (Input.touchSupported)
+            return GetPinch();
+        return 0;
+    }
+}
